256ccd11f06550e8acec7190015a002333ed173e
BisherRefai963 <201639133@fa-hists.edu.eg>
2023-03-20 16:25:26 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Discription : 'Check for number if it is positive or negative'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
