97ada07de31a838eb1734fee76f766918ddc649a
BisherRefai963 <201639133@fa-hists.edu.eg>
2023-03-21 12:08:54 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * main - Entry point
+ * Description : 'get last digit of number '
+ * Return: always 0
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
